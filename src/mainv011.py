1a19529faf7e05e169b832d27f643d77343421e7
FDFFDFDF <41364631+FDFFDFDF@users.noreply.github.com>
2022-11-20 19:10:01 +0900

    Add files via upload

--- src/mainv011.py
+++ src/mainv011.py
@@ -23,12 +23,14 @@ import os
 import logging
 import traceback
 import datetime
-from datetime import timedelta
-
-from tqdm import tqdm
-
 
 ### v0.1.1 추가 ###
+import csv
+from datetime import timedelta
+from tqdm import tqdm
+from tkinter import messagebox
+
+
 #내가 싼 라이브러리
 from Twitch_API import Twitch_API
 from Read_Clip_list_and_Downloader import Read_Clip_list_and_Downloader
@@ -38,19 +40,23 @@ from Read_Clip_list_and_Downloader import Read_Clip_list_and_Downloader
 if __name__ == '__main__':   
 
     #로그 설정
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
+    logger.handlers = []
     #logger.setLevel(logging.DEBUG)
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
     streamhandler = logging.StreamHandler()
     streamhandler.setFormatter(formatter)
     logger.addHandler(streamhandler)
-    filehandler = logging.FileHandler('logfile_{:%Y%m%d-%H_%M_%S}.log'.format(datetime.datetime.now()), encoding='utf-8')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    filehandler = logging.FileHandler('logs/logfile_{:%Y%m%d-%H_%M_%S}.log'.format(datetime.datetime.now()), encoding='utf-8')
     filehandler.setFormatter(formatter)
     logger.addHandler(filehandler)
 
     logger.info("시작")
-    Naver_res_lists=[]
+    res_lists=[]
 
     
 
@@ -126,7 +132,6 @@ if __name__ == '__main__':
             time.sleep(2)
 
             pyperclip.copy('')
-
 
             ## 네이버 카페 접속 및 클립 주소 얻기 ##
             Naver_res_lists = []
@@ -238,6 +243,8 @@ if __name__ == '__main__':
                 info_list = info_list + res_list[2]
 
             TAPI.save_clip_info(info_list, already_procd=True)
+            TAPI.boo = True
+            driver.close()
             ### v0.1.1 추가 end ###
 
             ''' v0.1.1 제거됨
@@ -427,12 +434,27 @@ if __name__ == '__main__':
         RCLD.Run(folder_by_streamer=False)
         ### v0.1.1 추가 end ###
 
-        
+
         logger.info("모든 클립 다운로드가 완료되었습니다.")
 
 
-        driver.close()
+        #driver.close()
 
     except:
-        logger.error(str(Naver_res_lists))
+        #logger.error(str(Naver_res_lists))
         logger.error(traceback.format_exc())
+
+        if traceback.format_exc().find("Connection to api.twitch.tv timed out")>=0:
+            messagebox.showwarning("타임아웃", "트위치 서버가 아파서 클립 정보를 받지 못했습니다.\n 이어서 진행하려면 프로그램을 바로 다시 실행해주세요")
+
+        else:
+            messagebox.showerror("치명적인 오류 발생", "알 수 없는 오류가 발생했습니다.\n 마지막으로 생성된 log 파일을 피드백 사이트에 보고해주세요")
+
+        if not TAPI.boo:     
+            csv_error_log = [(st + timedelta(hours=9)).strftime("%Y-%m-%d %H:%M:%S")] + ['error']*17
+
+            f_clips = open(Clip_file,'a', encoding='UTF8',newline="")       
+            wr = csv.writer(f_clips)
+            wr.writerow(csv_error_log)
+            f_clips.close()
+        
